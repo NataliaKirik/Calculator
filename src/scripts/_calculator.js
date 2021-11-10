7ed9960a4383a1837e0f9d0fc15d1b62c26f2908
Natalia <nataliya.krotava@innowise-group.com>
2021-11-10 11:08:18 +0300

    add method calculate operations

--- src/scripts/_calculator.js
+++ src/scripts/_calculator.js
@@ -1,6 +1,6 @@
 export class Calculator {
-    constructor(previousOperandTextElement, currentOperandTextElement) {
-        this.previousOperandTextElement = previousOperandTextElement
+    constructor(previousOperationElement, currentOperandTextElement) {
+        this.previousOperationElement = previousOperationElement
         this.currentOperandTextElement = currentOperandTextElement
         this.clearAll()
     }
@@ -8,22 +8,26 @@ export class Calculator {
     appendNumber(number) {
         if (number === '.' && this.currentOperand.includes('.')) return
         this.currentOperand = this.currentOperand + number
-        if (this.currentOperand.toString().length > 1 && this.currentOperand.toString().startsWith('0')) {
-            this.currentOperand = this.currentOperand.toString().slice(1)
+        if (this.currentOperand.startsWith('0') &&
+            this.currentOperand.length > 1 &&
+            !this.currentOperand.includes('.')
+        ) {
+            this.currentOperand = this.currentOperand.slice(1)
         }
     }
 
     chooseOperation(operation) {
         if (this.currentOperand === '') return
         if (this.previousOperand !== '') {
-            this.compute()
+            this.calculateSimpleOperations()
         }
         this.operation = operation
+        //add current operand to memory
         this.previousOperand = this.currentOperand
         this.currentOperand = ''
     }
 
-    compute() {
+    calculateSimpleOperations() {
         let result
         const prev = parseFloat(this.previousOperand)
         const current = parseFloat(this.currentOperand)
@@ -52,15 +56,38 @@ export class Calculator {
         this.previousOperand = ''
     }
 
-    displayData() {
-        this.currentOperandTextElement.innerText =
-            this.currentOperand
-        if (this.operation != null) {
-            this.previousOperandTextElement.innerText =
-                `${this.previousOperand} ${this.operation}`
-        } else {
-            this.previousOperandTextElement.innerText = ''
+    calculateOperations(operation) {
+        let result
+        this.operation = operation
+        const current = parseFloat(this.currentOperand)
+        if (isNaN(current)) return
+        switch (operation) {
+            case 'x²':
+                result = current ** 2
+                break
+            case 'x³':
+                result = current ** 3
+                break
+            case '1/x':
+                result = 1 / current
+                break
+            case '√x':
+                result = Math.sqrt(current)
+                break
+            case '∛x':
+                result = Math.cbrt(current)
+                break
+            case 'log':
+                result = Math.log(current)
+                break
+            case 'log10':
+                result = Math.log10(current)
+                break
+            default:
+                break
         }
+        this.currentOperand = result
+        this.operation = undefined
     }
 
     clearAll() {
@@ -71,5 +98,17 @@ export class Calculator {
 
     delete() {
         this.currentOperand = this.currentOperand.toString().slice(0, -1)
+    }
+
+    displayData() {
+        console.log(this.operation)
+        this.currentOperandTextElement.innerText =
+            this.currentOperand
+        if (this.operation) {
+            this.previousOperationElement.innerText =
+                `${this.previousOperand} ${this.operation}`
+        } else {
+            this.previousOperationElement.innerText = ''
+        }
     }
 }
--- src/scripts/calculator.js
+++ src/scripts/calculator.js
@@ -12,130 +12,6 @@ const memoryElement = document.querySelector('[data-memory]')
 const memoryButtons = document.querySelector('[data-memory-operation]')
 
 
-// let currentOperand = ''
-// let lastOperand = '';
-// let lastOperation = '';
-// let haveDot = false
-// let result = null
-// let memory = null
-// numberButtons.forEach((number) => {
-//     number.addEventListener("click", (event) => {
-//         if (event.target.innerText === "." && !haveDot) {
-//             haveDot = true;
-//         } else if (event.target.innerText === "." && haveDot) {
-//             return;
-//         }
-//         currentOperand += event.target.innerText;
-//         currentOperandTextElement.innerText = currentOperand;
-//     });
-// });
-// simpleOperationButtons.forEach((operation) => {
-//     operation.addEventListener("click", (event) => {
-//         if (!currentOperand) return;
-//         haveDot = false;
-//         const operationName = event.target.innerText;
-//         if (previousOperationElement && currentOperand && lastOperand) {
-//             simpleMathOperation();
-//         } else {
-//             result = parseFloat(currentOperand);
-//         }
-//         setCurrentNumberToMemory(operationName);
-//         lastOperation = operationName;
-//     });
-// });
-// operationButtons.forEach((operation) => {
-//     operation.addEventListener("click", (event) => {
-//         if (!currentOperand) return;
-//         haveDot = false;
-//         if (currentOperand) {
-//             mathOperation(event.target.innerText);
-//         } else {
-//             result = parseFloat(currentOperand);
-//         }
-//     });
-// });
-// function setCurrentNumberToMemory(name = "") {
-//     lastOperand += currentOperand + " " + name + " ";
-//     previousOperationElement.innerText = lastOperand;
-//     currentOperandTextElement.innerText = "";
-//     currentOperand = '';
-// }
-// function simpleMathOperation() {
-//     switch (lastOperation) {
-//         case '*':
-//             result = parseFloat(result) * parseFloat(currentOperand);
-//             break
-//         case '+':
-//             result = parseFloat(result) + parseFloat(currentOperand);
-//             break
-//         case '-':
-//             result = parseFloat(result) - parseFloat(currentOperand);
-//             break
-//         case '÷':
-//             result = parseFloat(result) / parseFloat(currentOperand);
-//             break
-//         case '%':
-//             result = parseFloat(result) % parseFloat(currentOperand);
-//             break
-//         default:
-//             break
-//     }
-//     lastOperand = result
-//     currentOperand = '';
-// }
-// function mathOperation(operation) {
-//     switch (operation) {
-//         case 'x²':
-//             result = parseFloat(currentOperand) ** 2
-//             break
-//         case 'x³':
-//             result = parseFloat(currentOperand) ** 3
-//             break
-//         case '1/x':
-//             result = 1 / parseFloat(currentOperand)
-//             break
-//         case '√x':
-//             result = Math.sqrt(parseFloat(currentOperand))
-//             break
-//         case '∛x':
-//             result = Math.cbrt(parseFloat(currentOperand))
-//             break
-//         case 'log':
-//             result = Math.log(parseFloat(currentOperand))
-//             break
-//         case 'log10':
-//             result = Math.log10(parseFloat(currentOperand))
-//             break
-//         default:
-//             break
-//     }
-//     currentOperandTextElement.innerText = result
-// }
-// equalButton.addEventListener('click', () => {
-//     if (!currentOperand || !lastOperand) return;
-//     haveDot = false;
-//     simpleMathOperation();
-//     setCurrentNumberToMemory();
-//     currentOperandTextElement.innerText = result;
-//     currentOperand = result;
-//     lastOperand = '';
-// });
-// allClearButton.addEventListener("click", () => {
-//     lastOperand = "";
-//     currentOperand = "";
-//     previousOperationElement.innerText = "";
-//     currentOperandTextElement.innerText = "";
-//     result = "";
-// });
-// deleteButton.addEventListener("click", () => {
-//     if (currentOperandTextElement.innerText.length) {
-//         currentOperandTextElement.innerText = currentOperandTextElement.innerText.slice(0, -1)
-//         currentOperand = currentOperandTextElement.innerText
-//     } else {
-//         currentOperand = ''
-//     }
-// });
-
 const calculator = new Calculator(previousOperationElement, currentOperandTextElement)
 
 numberButtons.forEach(button => {
@@ -150,8 +26,15 @@ simpleOperationButtons.forEach(button => {
         calculator.displayData()
     })
 })
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        calculator.calculateOperations(button.innerText)
+        calculator.displayData()
+    })
+})
 equalButton.addEventListener('click', () => {
-    calculator.compute()
+    calculator.calculateSimpleOperations()
+    calculator.calculateOperations()
     calculator.displayData()
 })
 allClearButton.addEventListener('click', () => {
